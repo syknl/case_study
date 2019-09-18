4d11e386c8eb4c1b9f7de6ff8fe9ed01ad206ae5
Sayaka Ishihara <syk_is@outlook.jp>
2019-09-18 23:38:46 +0200

    Before  dividing functions.

--- case_study/Program.cs
+++ case_study/Program.cs
@@ -8,37 +8,38 @@ namespace case_study_core
         public static void Main()
         {
             // Limit of name length.
-            const int length_limit = 20;
+            const int lengthLimit = 20;
 
-            string first_name = Get_input("What is your first name?", length_limit);
-            string last_name = Get_input("What is your last name?", length_limit);
+            string firstName = GetInput("What is your first name?", lengthLimit);
+            string lastName = GetInput("What is your last name?", lengthLimit);
 
-            // The maximum of the loop.
-            const int max_num = 100;
+            FizzbuzzLikeAll(firstName, lastName);
+            //for (int i = 1; i <= 100; i++)
+            //{ 
+            //    string strOut = FizzbuzzLikeEach(firstName, lastName, i);
+            //    Console.WriteLine(strOut);
 
-            for (int i = 1; i <= max_num; i++)
-            { 
-                string str_out = Fizzbuzz_like(first_name, last_name, i);
-                Console.WriteLine(str_out);
-
-            }
+            //}
         }
 
-        public static string Get_input(string message, int length_limit)
+        public static string GetInput(string message, int lengthLimit)
         {
             Console.WriteLine(message);
             string name = Console.ReadLine();
 
+            // todo: move to own method.
+            // todo: remove comment
             //Checking length of the names and exiting if too long.
             int len = name.Length;
-            if (len > length_limit)
+            if (len > lengthLimit)
             {
-                string str = "Please enter " + $"{length_limit}" + " or less number of characters.";
+                string str = "Please enter " + $"{lengthLimit}" + " or less number of characters.";
                 Console.WriteLine(str);
                 Environment.Exit(0);
 
             }
 
+            // todo: move to own method
             if (!Regex.IsMatch(name, @"^[a-zA-Z]+$")) {
                 Console.WriteLine("Please enter only alphabets.");
                 Environment.Exit(0);
@@ -47,11 +48,20 @@ namespace case_study_core
             return name;
         }
 
-        public static string Fizzbuzz_like(string first_name, string last_name, int num)
+        public static string FizzbuzzLikeEach(string firstName, string lastName, int num)
         {
-            string str1 = (num % 3 == 0 ? first_name : "") + (num % 15 == 0 ? " " : "") + (num % 5 == 0 ? last_name : "");
-            string str2 = (str1 == "" ? $"{num}" : str1);
-            return str2;
+            string str = (num % 3 == 0 ? firstName : "") + (num % 15 == 0 ? " " : "") + (num % 5 == 0 ? lastName : "");
+            return (str.Length == 0 ? $"{num}" : str);
+        }
+
+        public static void FizzbuzzLikeAll(string firstName, string lastName)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                var strOut = FizzbuzzLikeEach(firstName, lastName, i);
+                Console.WriteLine(strOut);
+            }
+            return;
         }
     }
 }
--- case_study_test/UnitTest1.cs
+++ case_study_test/UnitTest1.cs
@@ -1,24 +1,47 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.IO;
+using case_study_core;
 
 namespace case_study_test
 {
     [TestClass]
     public class UnitTest1
     {
+
+        const string mockFirstName = "Sayaka";
+        const string mockLastName = "Ishihara";
+
         [TestMethod]
-        public void TestMethod1()
+        public void TestFizzbuzzLikeEachWithNonDivisibleNumber()
         {
-            using (var sw = new StringWriter()) {
-                Console.SetOut(sw);
-                string Expected = "Gareth";
-                case_study_core.Program.Main();
-                
-                var result = sw.ToString().Trim();
-                Console.WriteLine(result);
-                Assert.AreEqual(Expected, result);
-            }
+            //using (var sw = new StringWriter()) {
+            //    Console.SetOut(sw);
+            //    string Expected = "Sayaka";
+            //    case_study_core.Program.Main();
+
+            //    var result = sw.ToString().Trim();
+            //    Console.WriteLine(result);
+            //    Assert.AreEqual(Expected, result);
+            //}
+
+            
+
+            var mockNumber = 1;
+            var expected = "1";
+
+            var fizzbuzzValue = Program.FizzbuzzLikeEach(mockFirstName, mockLastName, mockNumber);
+            Assert.AreEqual(expected, fizzbuzzValue);
+        }
+
+        [TestMethod]
+        public void TestFizzbuzzLikeEachWithDivisibleByThree()
+        {
+            var mockNumber = 3;
+            var expected = "Sayaka";
+
+            var fizzbuzzValue = Program.FizzbuzzLikeEach(mockFirstName, mockLastName, mockNumber);
+            Assert.AreEqual(expected, fizzbuzzValue);
         }
     }
 }
